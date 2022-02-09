7f9e2ea6e7d157f801eebed85330b2ccb6e67a95
Fatal1ty203 <85799432+Fatal1ty203@users.noreply.github.com>
2022-02-09 16:13:09 +0300

    add stream

---
+++ src/hw/homework_stream_part2/task1/Main.java
@@ -0,0 +1,32 @@
+package hw.homework_stream_part2.task1;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Book> bookStream = List.of(new Book("Ницше","По ту сторону добра и зла "),
+                new Book("Ницше", "Так говорил Заратустра"),
+                new Book("Путин", "Гарик потный"),
+                new Book("Сакс", "Человек, которы принял жену за шляпу"),
+                new Book("Бёрджесс", "Однорукий аплодисмент"),
+                new Book("Эппель", "Шампиньон моей жизни"));
+        Scanner scn = new Scanner(System.in);
+
+        int input = 0;
+        while (input!=2){
+            System.out.println("1. Найти книги автора: \n" +
+                    "2.Выход");
+            input= scn.nextInt();
+            scn.nextLine();
+            if (input==1){
+                String authorName = scn.nextLine();
+                List<String> stringList = bookStream.stream().filter(val -> val.getAuthorName().equalsIgnoreCase(authorName)).map(val -> val.getBookName()).collect(Collectors.toList());
+                System.out.println(stringList);
+            }
+        }
+
+    }
+}
