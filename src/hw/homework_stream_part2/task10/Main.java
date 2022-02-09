81eedded4c02e7084f6142214f44a2a958b52c78
Fatal1ty203 <85799432+Fatal1ty203@users.noreply.github.com>
2022-02-09 22:16:23 +0300

    update homework_stream_part2

--- src/hw/homework_stream_part2/task10/Main.java
+++ src/hw/homework_stream_part2/task10/Main.java
@@ -4,5 +4,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<Book> list = List.of(new Book("b1","a1",100),new Book("b2", "a2", 70));
+        List<Book> list2 = List.of(new Book("b3","a3", 80), new Book("b4","a4",120));
+        List<Library> libraries = List.of(new Library(list), new Library(list2));
+
     }
 }
