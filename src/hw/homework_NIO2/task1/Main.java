c5490f5135eacdf2f0b23d3feccdcb7040a53d92
Fatal1ty203 <85799432+Fatal1ty203@users.noreply.github.com>
2022-02-11 15:46:13 +0300

    add task2 - 3

---
+++ src/hw/homework_NIO2/task1/Main.java
@@ -0,0 +1,67 @@
+package hw.homework_NIO2.task1;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<String> stringList = List.of("Привет", "Как дела", "Ну пока");
+        String s = "Кто сражается с чудовищами, тому следует остерегаться, чтобы самому при этом не стать чудовищем. И если ты долго смотришь в бездну, то бездна тоже смотрит в тебя.";
+        int[] in = {8,800,555,35,35};
+
+        Path path = Path.of("src/hw/homework_NIO2/source/file1.txt");
+        Path path2 = Path.of("src/hw/homework_NIO2/source/file2.txt");
+        Path path3 = Path.of("src/hw/homework_NIO2/source/file3.txt");
+
+        Files.write(path,stringList, StandardOpenOption.CREATE);
+        Files.writeString(path2,s, StandardOpenOption.CREATE);
+        BufferedWriter bw = Files.newBufferedWriter(path3);
+
+        Arrays.stream(in)
+                .mapToObj(String::valueOf)
+                .forEach(value -> {
+                    try {
+                        bw.write(value);
+                        bw.newLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+        bw.close();
+
+        List<Path> list = new ArrayList<>();
+        Path pathSrc = Path.of("src/hw/homework_NIO2/source");
+        Path pathDir = Path.of("src/hw/homework_NIO2/dest");
+        finedFiles(pathSrc, list);
+
+        for (Path st : list){
+            File file = new File(String.valueOf(pathDir), st.toString());
+            String pSrc = pathSrc.toString() +"\\" + st;
+            String pDir = pathDir.toString() +"\\" + "copyOf" + st;
+            System.out.println(pSrc +" " + pDir);
+            Files.copy(Path.of(pSrc), Path.of(pDir), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+    }
+
+
+    //Находим все файлы в указанном каталоге
+    public static void finedFiles(Path source, List<Path> list){
+        File dir = new File(source.toString());
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++){
+            if (files[i] != null && files[i].isFile()){ //проверяем, что ячейка массива файлов не пустая и является файлом
+                list.add(Path.of(files[i].getName())); // добавляем в список имена файлов из данного каталога
+
+            }
+        }
+    }
+}
--- src/lessons/lesson37_stream/Main2.java
+++ src/lessons/lesson37_stream/Main2.java
@@ -14,7 +14,11 @@ public class Main2 {
         };
 
         //Stream<int[]> stream = Arrays.stream(data); //стрим по двумерному массиву, это стрим по его одномерным массивам
-        long count = Arrays.stream(data).map(arr -> Arrays.stream(arr).sum()).filter(sum -> sum > 10).count();
+        long count = Arrays.stream(data)
+                .map(arr -> Arrays.stream(arr)
+                        .sum())
+                .filter(sum -> sum > 10)
+                .count();
         System.out.println(count);
     }
 }
